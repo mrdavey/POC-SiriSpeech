7bd5638e85f0880aebc290e8f087bcb22c9a2516
David Truong <mrdavey@users.noreply.github.com>
2016-11-02 13:41:35 +0100

    Initial project upload
    Supports English and Dutch

---
+++ POC-siri-speech/ViewController.swift
@@ -0,0 +1,165 @@
+//
+//  ViewController.swift
+//  POC-siri-speech
+//
+//  Created by David Truong | The Mobile Company on 02/11/2016.
+//  Copyright © 2016 David Truong. All rights reserved.
+//
+
+import UIKit
+import Speech
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var micButton: UIButton!
+    @IBOutlet weak var englishButton: UIButton!
+    @IBOutlet weak var dutchButton: UIButton!
+
+    private var speechRecognizer = SFSpeechRecognizer(locale: Locale.init(identifier: "en-US"))
+    private var recognitionRequest: SFSpeechAudioBufferRecognitionRequest?
+    private var recognitionTask: SFSpeechRecognitionTask?
+    private let audioEngine = AVAudioEngine()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        micButton.isEnabled = false
+
+        speechRecognizer?.delegate = self
+
+        SFSpeechRecognizer.requestAuthorization { (authStatus) in
+
+            var isButtonEnabled = false
+
+            switch authStatus {
+            case .authorized:
+                isButtonEnabled = true
+
+            case .denied:
+                isButtonEnabled = false
+                print("User denied access to speech recognition")
+
+            case .restricted:
+                isButtonEnabled = false
+                print("Speech recognition restricted on this device")
+
+            case .notDetermined:
+                isButtonEnabled = false
+                print("Speech recognition not yet authorized")
+            }
+
+            OperationQueue.main.addOperation() {
+                self.micButton.isEnabled = isButtonEnabled
+            }
+        }
+
+        /*
+         
+         Custom user-facing speech authorisation messages are in info.plist file
+ 
+        */
+    }
+
+    @IBAction func englishButtonPressed(_ sender: Any) {
+        speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "en-US"))
+    }
+
+    @IBAction func dutchButtonPressed(_ sender: Any) {
+        speechRecognizer = SFSpeechRecognizer(locale: Locale(identifier: "nl-NL"))
+    }
+
+
+    @IBAction func micButtonTapped(_ sender: Any) {
+        if audioEngine.isRunning {
+            audioEngine.stop()
+            recognitionRequest?.endAudio()
+            micButton.isEnabled = false
+            micButton.setTitle("Start Recording", for: .normal)
+        } else {
+            startRecording()
+            micButton.setTitle("Stop Recording", for: .normal)
+        }
+    }
+
+    //
+    //  Handle the recording and sending to Apple for transcription
+    //
+
+    func startRecording() {
+
+        if recognitionTask != nil {
+            recognitionTask?.cancel()
+            recognitionTask = nil
+        }
+
+        let audioSession = AVAudioSession.sharedInstance()
+        do {
+            try audioSession.setCategory(AVAudioSessionCategoryRecord)
+            try audioSession.setMode(AVAudioSessionModeMeasurement)
+            try audioSession.setActive(true, with: .notifyOthersOnDeactivation)
+        } catch let error as NSError {
+            print("audioSession properties weren't set: \(error.localizedDescription)")
+        }
+
+        recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
+
+        // Device needs to have a microphone
+        guard let inputNode = audioEngine.inputNode else {
+            fatalError("Audio engine has no input node")
+        }
+
+        guard let recognitionRequest = recognitionRequest else {
+            fatalError("Unable to create an SFSpeechAudioBufferRecognitionRequest object")
+        }
+
+        recognitionRequest.shouldReportPartialResults = true
+
+        recognitionTask = speechRecognizer?.recognitionTask(with: recognitionRequest, resultHandler: { (result, error) in
+            var isFinal = false
+
+            if result != nil {
+
+                self.textView.text = result?.bestTranscription.formattedString
+                isFinal = (result?.isFinal)!
+            }
+
+            if error != nil || isFinal {
+                self.audioEngine.stop()
+                inputNode.removeTap(onBus: 0)
+
+                self.recognitionRequest = nil
+                self.recognitionTask = nil
+
+                self.micButton.isEnabled = true
+            }
+        })
+
+        let recordingFormat = inputNode.outputFormat(forBus: 0)
+        inputNode.installTap(onBus: 0, bufferSize: 1024, format: recordingFormat) { (buffer, when) in
+            self.recognitionRequest?.append(buffer)
+        }
+
+        audioEngine.prepare()
+        
+        do {
+            try audioEngine.start()
+        } catch {
+            print("audioEngine couldn't start because of an error.")
+        }
+        
+        textView.text = "Say something, I'm listening!"
+    }
+}
+
+extension ViewController: SFSpeechRecognizerDelegate {
+
+    func speechRecognizer(_ speechRecognizer: SFSpeechRecognizer, availabilityDidChange available: Bool) {
+        if available {
+            micButton.isEnabled = true
+        } else {
+            micButton.isEnabled = false
+        }
+    }
+}
+
